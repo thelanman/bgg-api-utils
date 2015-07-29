809be4104fbd89d570d2a59053eb3523012847ff
thelanman <lanahan.j@gmail.com>
2015-07-29 18:06:21 -0400

    added unicode support for printing names

--- api.py
+++ api.py
@@ -1,10 +1,9 @@
 import urllib2
 import xml.etree.ElementTree as ET
 
-from utils import Throttle, get_xml_subelement
+from utils import get_xml_subelement, Throttle, load_xml_from_file
 
-
-@Throttle(sleep=(1,4))
+@Throttle(sleep=(3, 8))
 def get_xml_games_api(gid, path=None):
     url = 'https://www.boardgamegeek.com/xmlapi2/thing?type=boardgame&id=%s&stats=1'
     if type(gid) is not list:
@@ -25,7 +24,7 @@ def get_xml_games_api(gid, path=None):
 
 
 def bulk_game_retriever(start, stop, path=None):
-    inc = min(99, stop - start)
+    inc = min(99, stop - start + 1)
     for s in range(start, stop, inc):
         print s
         get_xml_games_api(range(s, s + inc), path)
@@ -34,8 +33,10 @@ def bulk_game_retriever(start, stop, path=None):
 class Game(object):
     def __init__(self, xml_elem):
         self.id = int(xml_elem.attrib['id'])
+        self.type = xml_elem.attrib['type']
         self.names_alt = get_xml_subelement(xml_elem, ".//name[@type='alternate']", 'value', True)
         self.name = get_xml_subelement(xml_elem, ".//name[@type='primary']", 'value')
+        self.name = unicode(self.name).encode('utf8')
         self.min_players = get_xml_subelement(xml_elem, 'minplayers', 'value', convert=int)
         self.max_players = get_xml_subelement(xml_elem, 'maxplayers', 'value', convert=int)
         self.min_age = get_xml_subelement(xml_elem, 'minage', 'value', convert=int)
@@ -52,6 +53,7 @@ class Game(object):
         self.playingtime = get_xml_subelement(xml_elem, 'playingtime', 'value', convert=int)
         self.year_published = get_xml_subelement(xml_elem, 'yearpublished', 'value', convert=int)
         self.description = get_xml_subelement(xml_elem, 'description')
+        self.description = unicode(self.description).encode('utf8')
         self.categories = get_xml_subelement(xml_elem, ".//link[@type='boardgamecategory']", 'value', True)
         self.families = get_xml_subelement(xml_elem, ".//link[@type='boardgamefamily']", 'value', True)
         self.mechanics = get_xml_subelement(xml_elem, ".//link[@type='boardgamemechanic']", 'value', True)
@@ -65,6 +67,7 @@ class Game(object):
                 for result in results:
                     self.suggested_numplayers[numplayers][result.attrib['value']] = int(result.attrib['numvotes'])
             self.calc_suggested_numplayers()
+        self.suggested_numplayers_best = self.suggested_numplayers.get('Best', None)
         self.ratings = {}
         ratings = xml_elem.find('statistics').find('ratings')
         self.ratings['usersrated'] = get_xml_subelement(ratings, 'usersrated', 'value', convert=int)
@@ -83,9 +86,35 @@ class Game(object):
         self.ranks = get_xml_subelement(ranks, 'rank', ['bayesaverage', 'friendlyname', 'id', 'name', 'type', 'value'], True, quiet=True)
         self.rank = get_xml_subelement(ranks, ".//rank[@name='boardgame']", 'value', convert=int, quiet=True)
 
+    def to_csv(self, header):
+        return ','.join('"%s"' % self.__dict__[h] for h in header.split(','))
+
     def calc_suggested_numplayers(self):
         for s in ['Best', 'Recommended', 'Not Recommended']:
             self.suggested_numplayers[s] = max(self.suggested_numplayers.items(), key=lambda p: p[1][s] if p[0] not in ['totalvotes', 'Best', 'Recommended', 'Not Recommended'] else -10000)[0]
 
     def __repr__(self):
-        return 'Game(%s : %s)' % (self.name, self.id)
+        return 'Game(%s id:%s rank:%s)' % (self.name, self.id, self.rank)
+
+
+def load_games(gid, path, silent=True):
+    if type(gid) is not list:
+        gid = [gid]
+    games = []
+    for g in gid:
+        try:
+            games += [Game(load_xml_from_file(path + 'bgg_game_%06d.xml' % g))]
+        except:
+            if not silent:
+                raise
+    return games
+
+
+def game_to_csv(games, fn, header=None):
+    if header is None:
+        header = 'id,name,min_players,suggested_numplayers_best,max_players,min_age,avg_age,year_published,rank,playingtime,mechanics,categories'
+    with open(fn, 'w') as f:
+        f.write(header + '\n')
+        for g in games:
+            f.write(g.to_csv(header) + '\n')
+
